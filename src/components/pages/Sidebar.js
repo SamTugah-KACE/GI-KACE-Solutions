ad972788881493633aeef02a857a8bb7a8c5e13c
SAM-TUGAH <samuel.kusi-duah@gi-kace.gov.gh>
2025-03-01 15:19:27 +0000

    logout-func-sidebar

--- src/components/pages/Sidebar.js
+++ src/components/pages/Sidebar.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import './Sidebar.css';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import request from './request';
+import request from '../request';
 import LogoutConfirmationModal from './LogoutConfirmationModal';
 
 
