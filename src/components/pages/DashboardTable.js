9437d6ea278bf0747a9e336703871a52cf1eead6
SamTugah-KACE <samuel.kusi-duah@gi-kace.gov.gh>
2025-06-12 14:01:14 +0000

    bfhvkjk

--- src/components/pages/DashboardTable.js
+++ src/components/pages/DashboardTable.js
@@ -293,12 +293,12 @@ const DashboardTable = ({ orgId, token }) => {
         <tbody>
            {wsError && <div className="ws-error">{wsError}</div>}
           {data.map((row, idx) => {
-            const accountName = row["account_name"];
-            const role        = row.role;
-            const issues      = row.issues;
-            const attachments = row.attachments || [];
-            const actions     = row.actions;
-            const record = row.data;
+            const accountName = row["Account Name"];
+            const role        = row.Role;
+            const issues      = row.Issues;
+            const attachments = row.Attachments || [];
+            const actions     = row.Actions;
+            const record = row.Data;
 
             return (
               <tr key={idx}>
